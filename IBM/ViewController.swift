543744e3fd4d52b323acfe790fa31c5a9d43426f
Alessandro Hecht <alessandrohc@gmail.com>
2017-05-28 12:34:27 -0300

    Projeto simples em switch para abrir um url do webapp

--- IBM/ViewController.swift
+++ IBM/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let urlPath: String = "http://10.211.55.9:8000"
+        let URL: NSURL = NSURL(string: urlPath)!
+        let requestObj: NSURLRequest = NSURLRequest(url: URL as URL)
+        
+        webView.loadRequest(requestObj as URLRequest);
+
     }
 
     override func didReceiveMemoryWarning() {
